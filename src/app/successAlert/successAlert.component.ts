a4740c63752e42424c1ca3c1f667ab2d54bccacf
Yury Krasavin <buggins@live.com>
2018-10-18 19:04:25 +0300

    add warning and success alert components

--- src/app/successAlert/successAlert.component.ts
+++ src/app/successAlert/successAlert.component.ts
@@ -2,6 +2,6 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "app-success-alert",
-  template: "<p>This is SuccesAlert component!"
+  template: "<p>This is SuccesAlert component!</p>"
 })
 export class SuccessAlertComponent {}
--- src/app/warningAlert/warningAlert.component.ts
+++ src/app/warningAlert/warningAlert.component.ts
@@ -2,6 +2,6 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "app-warning-alert",
-  template: "<p>This is WarningAlert component!"
+  template: "<p>This is WarningAlert component!</p>"
 })
 export class WarningAlertComponent {}
