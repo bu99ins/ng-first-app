3644d5d94e740945617d6be5cbe22ad82dc5bad5
Yury Krasavin <buggins@live.com>
2018-12-06 03:32:40 +0300

    Add ingredients to a recipe.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,19 +1,22 @@
 import { Recipe } from "./recipe.model";
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
     new Recipe(
-      "A Test Recipe",
-      "This is simply a test",
-      "https://www.skinnytaste.com/wp-content/uploads/2018/02/breakfast-pizza-1-8.jpg"
+      "Tasty Schnitzel",
+      "A super-tasty schnitzel — just awesome!",
+      "https://upload.wikimedia.org/wikipedia/commons/c/c8/J%C3%A4gerschnitzel_IMG_0936.jpg",
+      [new Ingredient("Meat", 1), new Ingredient("French Fries", 20)]
     ),
     new Recipe(
-      "Another Test Recipe",
-      "This is simply a test",
-      "https://www.skinnytaste.com/wp-content/uploads/2018/02/breakfast-pizza-1-8.jpg"
+      "Big Fat Burger",
+      "What else you need to say?",
+      "https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg",
+      [new Ingredient("Buns", 2), new Ingredient("Meat", 1)]
     )
   ];
 
