2bf603106ae37101cf95889cb2cf4c12870c37b0
Yury Krasavin <buggins@live.com>
2019-02-05 23:51:26 +0300

    Make shopping list items selectable

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -24,6 +24,10 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
     );
   }
 
+  onEditItem(index: number) {
+    this.slService.srartedEditing.next(index);
+  }
+
   ngOnDestroy() {
     this.subscribtion.unsubscribe();
   }
